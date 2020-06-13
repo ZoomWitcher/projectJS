2561acca84f56ddbddb49dfd6a37f7e6efead41f
Andrey <k2-Witcher@mail.ru>
2020-06-13 22:10:23 +0600

    DZ1

--- JS/script.js
+++ JS/script.js
@@ -2,9 +2,29 @@
 //alert("hi bitsh");
 //const result = confirm("?");
 //const answer = prompt("?","18");
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt("?","18");
-answers[1] = prompt("?","18");
-answers[2] = prompt("?","18");
-document.write(answers);
+// answers[0] = prompt("?","18");
+// answers[1] = prompt("?","18");
+// answers[2] = prompt("?","18");
+// document.write(answers);
+//let number0fFilms;
+
+const number0fFilms = +prompt("?");
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies:{},
+    actors:{},
+    genres: [],
+    privat:false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
