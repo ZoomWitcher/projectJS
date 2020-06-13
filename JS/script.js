07086d29e4a99b9d94d068944e3e8a6a6af39d08
Andrey <k2-Witcher@mail.ru>
2020-06-13 20:52:14 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+"use strict";
+//alert("hi bitsh");
+//const result = confirm("?");
+//const answer = prompt("?","18");
+const answers = [];
+
+answers[0] = prompt("?","18");
+answers[1] = prompt("?","18");
+answers[2] = prompt("?","18");
+document.write(answers);
